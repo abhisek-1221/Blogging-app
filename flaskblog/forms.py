4a60afd5a558489a7a152de2c9aa9fbc7fda1e78
abhisek1221 <cherrymx317@gmail.com>
2023-08-29 23:53:59 +0530

    User Authentication

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -14,13 +14,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken! Please use another username')
 
     def validate_email(self, email):
-
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken! Please use another email')
